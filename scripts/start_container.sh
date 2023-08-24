b22b4cbe74d5c346cda0933de0f4178d3f1c429d
Benjamin Dutta <39235568+Benjamindutta@users.noreply.github.com>
2023-08-24 10:21:25 +0530

    added appspec and scripts for running containers

---
+++ scripts/start_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+echo "starting the docker compose"
+
+# Run the Docker image as a container
+docker compose up
---
+++ scripts/stop_container.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+
+# Stop the Docker image as a container
+docker compose down
